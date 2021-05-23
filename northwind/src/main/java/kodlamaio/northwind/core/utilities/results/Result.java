1252093a134bfb20c16a7b076c4e4ec23b7edb88
nadissu <nadidesigirtmac@gmail.com>
2021-05-23 16:49:53 +0300

    Swagger and JPA

---
+++ northwind/src/main/java/kodlamaio/northwind/core/utilities/results/Result.java
@@ -0,0 +1,25 @@
+package kodlamaio.northwind.core.utilities.results;
+
+public class Result {
+	private boolean success;
+	private String message;
+	
+	public Result(boolean success) {
+		this.success=success;
+	}
+	public Result(String message) {
+		this.message=message;
+	}
+	
+public Result(boolean success,String message) {
+		this.success=success;
+		this.message=message;
+	}
+
+ public boolean isSuccess() {
+	 return this.success;
+ }
+ public String getMessage() {
+	 return this.message=message;
+ }
+}
